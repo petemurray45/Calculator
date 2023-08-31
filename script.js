53e51672ab1875f48f9988f6f20471e0833d6432
petemurray45 <pmurray697@gmail.com>
2023-08-31 20:39:22 +0100

    Started again - correctly assigned buttons

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
 //DOM selectors
 
+const clearBtn = document.querySelector('[data-all-clear]')
+const deleteBtn = document.querySelector('[data-delete]')
 const numberBtn = document.querySelectorAll('[data-number]')
 const operationBtn = document.querySelectorAll('[data-operation]')
-const deleteBtn = document.querySelector('[data-delete]')
-const clearBtn = document.querySelector('[data-all-clear]')
 const equalsBtn = document.querySelector('[data-equals]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
-const display = document.getElementsByClassName('current-operand')
-const keys = document.querySelector('.keys')
-const key = e.target;
-const action = key.dataset.action
-// classes and constructors to be used to store data
+const prevOperand = document.querySelector('[data-previous-operand]')
+const currentOperand = document.querySelector('[data-current-operand]')
 
-// event listeners 
 
-keys.addEventListener('click', e => {
-    if (e.target.matches('button')){
-        if(!action){
-            console.log('numberKey')
-        }
+
+
+// event listeners for buttons - to know each is assigned properly
+
+clearBtn.addEventListener('click', e => {
+    if(e.target.matches('button')){
+        console.log('clearBtn')
+        
+    }
+})
+
+deleteBtn.addEventListener('click', e => {
+    if(e.target.matches('button')){
+        console.log('deleteBtn')
+    }
+})
+
+equalsBtn.addEventListener('click', e => {
+    if(e.target.matches('button')){
+        console.log('equalsBtn')
 
     }
 })
+
+// using a loop to select number and operation buttons as they form an array
+
+for(i = 0; i < numberBtn.length; i++){
+    numberBtn[i].addEventListener('click', e => {
+        if(e.target.matches('button')){
+            console.log('numberBtn')
+        }
+    })
+
+}
+
+for(i = 0; i < operationBtn.length; i++){
+    operationBtn[i].addEventListener('click', e => {
+        if(e.target.matches('button')){
+            console.log('operationBtn')
+        }
+    })
+}
+
 

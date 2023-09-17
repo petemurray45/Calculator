fa9f7edbab742282c8e3498f38dadae28afb2b2c
petemurray45 <pmurray697@gmail.com>
2023-09-17 20:52:25 +0100

    New method of using calculator class

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+class Calculator {
+    constructor(previousNumberTextElement, currentNumberTextELement){
+        this.previousNumberTextElement = previousNumberTextElement
+        this.currentNumberTextELement = currentNumberTextELement
+
+    }
+
+    clear() {
+        this.currentNumber = " "
+        this.previousNumber = " "
+        this.operation = undefined;
+
+    }
+
+    delete(){
+
+
+    }
+
+    appendNumber(number){
+        if(number === ".") return
+        this.currentNumber = this.currentNumber.toString() + number.toString()
+
+
+    }
+
+    chooseOperation(operation){
+
+
+    }
+
+    compute(){
+
+
+    }
+
+    updateDisplay(){
+        this.currentNumberTextELement.innerText = this.currentNumber; 
+
+
+
+    }
+
+
+
+}
+
+
+
 //DOM selectors
 const numberBtn = document.querySelectorAll('[data-number]')
 const operationBtn = document.querySelectorAll('[data-operation]')
@@ -6,12 +55,18 @@ const clearBtn = document.querySelector('[data-all-clear]');
 const deleteBtn = document.querySelector('[data-delete]');
 const previousNumberTextElement = document.querySelector('[data-previous-number]')
 const currentNumberTextELement = document.querySelector('[data-current-number]')
+const calculator = new Calculator(previousNumberTextElement, currentNumberTextELement)
 
 
 
 
-// event listeners for buttons - to know each is assigned properly
 
+numberBtn.forEach(button => {
+    button.addEventListener("click", () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
 
 
 
